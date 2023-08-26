6fc8ad660f72d6fbf1d21d88986dddf8191dae13
Diego581 <diego445.gonzalez@gmail.com>
2023-08-26 18:05:14 -0300

    Firts-Commit

---
+++ src/app/message.service.ts
@@ -0,0 +1,17 @@
+import { JsonPipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
